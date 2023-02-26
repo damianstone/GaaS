1b200ff742aba2b41e1d1c0288650a5c816a0e13
Tiberiu Aurelian Țoca <78236499+ReverseZoom2151@users.noreply.github.com>
2023-02-26 23:39:56 +0000

    frontend 11:39 update

--- gaas-app/src/UserProfile.jsx
+++ gaas-app/src/UserProfile.jsx
@@ -1,82 +1,77 @@
-import { useState } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-// Initialize Firebase
-const firebaseConfig = {
-  // Add your Firebase config here
-};
+const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-firebase.initializeApp(firebaseConfig);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("/api/user");
+        setUser(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
 
-function UserProfile() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleSignUp = async () => {
-    try {
-      await firebase.auth().createUserWithEmailAndPassword(email, password);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const handleLogin = async () => {
-    try {
-      await firebase.auth().signInWithEmailAndPassword(email, password);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
+    fetchUserData();
+  }, []);
 
   const handleLogout = async () => {
     try {
-      await firebase.auth().signOut();
+      await axios.post("/api/logout");
+      setUser(null);
     } catch (error) {
-      setError(error.message);
+      console.error(error);
     }
   };
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  if (error) {
+    return <p>
+        An error occurred: {error.message}
+          </p>;
+  }
 
-  const isLoggedIn = firebase.auth().currentUser !== null;
+  if (!user) {
+    return (
+      <div>
+        <p>
+            Please log in to view your profile.
+        </p>
+        <a href="/login">
+            Log In
+        </a>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {isLoggedIn ? (
-        <div>
-          <p>You are logged in!</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <p>Please log in or sign up</p>
-          <div>
-            <label>Email:</label>
-            <input type="text" onChange={handleEmailChange} value={email} />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input type="password" onChange={handlePasswordChange} value={password} />
-          </div>
-          <div>
-            <button onClick={handleLogin}>Log in</button>
-            <button onClick={handleSignUp}>Sign up</button>
-          </div>
-          {error && <p>{error}</p>}
-        </div>
-      )}
+      <h1>
+        Welcome, {user.displayName}!
+      </h1>
+      <p>
+        Email: {user.email}
+      </p>
+      <p>
+        ID: {user.uid}
+      </p>
+      <button onClick={handleLogout}>
+        Log Out
+      </button>
     </div>
   );
-}
+};
 
 export default UserProfile;
 
 // This implementation uses the useState hook to store the email and password inputs, and also any error messages that might occur during authentication. It provides three functions for sign-up, login, and logout, which call the Firebase authentication API using the provided email and password inputs. The currentUser property of the Firebase auth object is used to determine if the user is currently logged in, and display the appropriate UI.
+// This version uses the axios library to make HTTP requests to the server instead of Firebase's SDK. The fetchUserData function makes a GET request to the /api/user endpoint to retrieve the user data, and the handleLogout function makes a POST request to the /api/logout endpoint to log the user out.
