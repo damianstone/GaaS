24a3bf5a47ff431e1fdb280917563578e0c1d520
Damian Stone <damianstonedev@gmail.com>
2023-02-27 01:44:14 +0000

    fix router

--- gaas-app/src/App.js
+++ gaas-app/src/App.js
@@ -1,33 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import './App.css';
+import Home from './pages/home/Home';
 
+import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          {/* 
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/login' component={LoginScreen} />
+      <Switch>
+        <Route path='/' component={Home} />
+        {/* <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} /> 
           <Route path='/myprofile' component={RegisterScreen} /> 
           <Route path='/proposals' component={RegisterScreen} /> 
           <Route path='/proposals:id' component={RegisterScreen} /> 
           <Route path='/create-proposal' component={RegisterScreen} /> 
           <Route path='/view-members' component={RegisterScreen} /> 
-          <Route path='/view-members:id' component={RegisterScreen} /> 
-          */}
-        </Container>
-      </main>
-      <Footer />
+          <Route path='/view-members:id' component={RegisterScreen} />  */}
+      </Switch>
     </Router>
   );
 };
